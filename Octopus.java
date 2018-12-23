f0fda5d930b645b2213bc3be2a7c1566603cdf99
chvuircitloorv <chvuircitloorv@gmail.com>
2018-12-23 15:47:26 +0300

    Create Octopus.java

---
+++ Octopus.java
@@ -0,0 +1,15 @@
+public class Octopus {
+
+    private char first = 234;
+    private char second = 228;
+    private char third = 241;
+    private char fourth = 236;
+    private char fifth = 231;
+    private char  sixth = 232;
+
+    public String encode() {
+        char[] word = { first, second, third, fourth, fifth, sixth};
+
+        return String.valueOf(word);
+    }
+}
